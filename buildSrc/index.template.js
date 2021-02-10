618000ad25b50096070a9411f286461cf36d6a15
ivk <ivk@tutao.de>
2021-02-10 14:02:06 +0100

    Browser support changes, close #2695
    Drop polyfills, drop some client detection, better start error message

---
+++ buildSrc/index.template.js
@@ -0,0 +1,43 @@
+function unsupported(e) {
+	console.error("Failed to import the app", e)
+
+	const img = document.createElement("img")
+	img.src = "images/logo-solo-red.svg"
+	img.style.display = "block"
+	img.style.margin = "0 auto"
+	document.body.appendChild(img)
+
+	const h1 = document.createElement("h1")
+	h1.innerText = "Tutanota"
+	h1.style.fontFamily = "sans-serif"
+	h1.style.fontSize = "40px"
+	h1.style.textAlign = "center"
+	document.body.appendChild(h1)
+
+	const div = document.createElement("div")
+	div.style.fontFamily = "sans-serif"
+	div.style.textAlign = "center"
+	div.style.fontSize = "24px"
+	document.body.appendChild(div)
+
+	const text = document.createElement("p")
+	text.textContent = "Seems like your browser is not supported or outdated. Please see FAQ entry for more information"
+	div.appendChild(text)
+
+	const link = document.createElement("a")
+	link.href = "https://tutanota.com/faq/#browser-support"
+	link.target = "_blank"
+	link.textContent = link.href
+	div.appendChild(link)
+
+	const err = document.createElement("pre")
+	err.textContent = navigator.userAgent + "\n" + e.toString() + "\n" + e.stack
+	div.appendChild(err)
+}
+
+try {
+	System.import('./app.js')
+	      .catch(unsupported)
+} catch (e) {
+	unsupported(e)
+}

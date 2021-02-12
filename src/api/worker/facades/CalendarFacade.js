e502944e839d587c134a2f4d0625dc146075ecea
ivk <ivk@tutao.de>
2021-02-12 16:18:23 +0100

    Remove unsafe Promise operations
    Functions Promise.prototype.{each, map, filter, reduce, spread} (not to
    be mistaken with "static" Promise.each, Promise.map etc) are not
    type-safe in a sense that they are only defined for Promisees which
    contain arrays but this behavior cannot be expressed for type-checked
    so the result type of them is effectively `any`.
    
    We remove them as they introduce dangerous gaps in type safety and also
    as a part of departing from Bluebird.

--- src/api/worker/facades/CalendarFacade.js
+++ src/api/worker/facades/CalendarFacade.js
@@ -248,7 +248,7 @@ export class CalendarFacade {
 			return this._entity.loadAll(UserAlarmInfoTypeRef, alarmInfoList.alarms)
 			           .then((userAlarmInfos) =>
 				           Promise
-					           .map(userAlarmInfos, (userAlarmInfo) =>
+					           .mapSeries(userAlarmInfos, (userAlarmInfo) =>
 						           this._entity.load(CalendarEventTypeRef, [
 							           userAlarmInfo.alarmInfo.calendarRef.listId, userAlarmInfo.alarmInfo.calendarRef.elementId
 						           ])
@@ -265,7 +265,7 @@ export class CalendarFacade {
 							               console.warn("NotAuthorized when downloading alarm event", e)
 							               return null
 						               }))
-					           .filter(Boolean) // filter out orphan alarms
+					           .then((alarms) => alarms.filter(Boolean)) // filter out orphan alarms
 			           )
 		} else {
 			console.warn("No alarmInfo list on user")
